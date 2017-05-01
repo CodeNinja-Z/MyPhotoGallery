36d5013894045956a0862a696dbcc352912c90c9
Carlos Zhao <blacklight728@gmail.com>
2017-05-01 16:44:07 -0400

    Add landing layout and view for OAuth log in.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,13 @@
 class PhotosController < ApplicationController
+  # Rails 5 changed the skip_xxx_action implementation.
+  # Adding raise: false to avoid the method not defined error.
+  # Ref: https://github.com/plataformatec/devise/issues/4207
+  skip_before_action :ensure_logged_in, only: [:landing], raise: false
+  
+  def landing
+    render template: 'photos/log_in', layout: 'layouts/landing_page'
+  end
+
   def index
     response = Photo.get_top_hundred
     data = JSON.parse(response)
