acd9dd166267152c82f823440bfb566bb5c7b109
Carlos Zhao <blacklight728@gmail.com>
2017-05-02 08:09:50 -0400

    Refactor likes controller.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,7 +8,9 @@ class LikesController < ApplicationController
     # 'voted' boolean variable, this is an alternative of checking
     # if a photo is liked and make sure the user can't like the 
     # same photo twice.
-    unless current_user.likes.where(photo_id: params[:photo_id]).first
+
+    # unless current_user.likes.find_by(photo_id: params[:photo_id])
+    unless liked_photo.present?
       Like.like_photo(current_user.id, params[:photo_id])
     end
     redirect_back(fallback_location: photos_path)
@@ -16,9 +18,14 @@ class LikesController < ApplicationController
 
   def delete
     # Same as above stated
-    if current_user.likes.where(photo_id: params[:photo_id]).first
+    if liked_photo.present?
       Like.unlike_photo(current_user.id, params[:photo_id])
     end
     redirect_back(fallback_location: photos_path)
+  end
+
+  private
+  def liked_photo
+    liked_photo = current_user.likes.select { |like| like.photo_id == params[:photo_id] }.first
   end
 end
