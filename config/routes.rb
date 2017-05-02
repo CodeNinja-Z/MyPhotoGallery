49be47cdda854a97f79b38d13d33d0bbe8961e2c
Carlos Zhao <blacklight728@gmail.com>
2017-05-02 01:45:33 -0400

    Add routes for like actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'photos#landing'
 
+  resources :photos, only: [:index]
+  post '/photos/:photo_id/like', to: 'likes#create', as: 'like_photo'
+  delete '/photos/:photo_id/unlike', to: 'likes#delete', as: 'unlike_photo'
+
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/auth/failure', to: redirect('/'), via: [:get]
   match '/signout', to: 'sessions#destroy', as: 'sign_out', via: [:delete]
-
-  resources :photos, only: [:index]
 end
