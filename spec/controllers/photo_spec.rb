5b4f42d980829b559c82062d1e9f85ec2c95ae96
Carlos Zhao <blacklight728@gmail.com>
2017-05-02 10:52:32 -0400

    Add photos controller show action test. Record API call with VCR.

--- spec/controllers/photo_spec.rb
+++ spec/controllers/photo_spec.rb
@@ -1,6 +1,7 @@
 describe PhotosController, type: :controller do
   describe '#index' do
     render_views
+
     before do
       VCR.use_cassette 'controllers/photos_index' do
         get :index
@@ -26,5 +27,24 @@ describe PhotosController, type: :controller do
     it 'display the template' do
       expect(response).to render_template :log_in
     end 
+  end
+
+  describe '#show' do
+    render_views
+
+      before { 
+        VCR.use_cassette 'controllers/photos_show' do
+          # photo with id 210026037
+          get :show, params: { id: '210026037' } 
+        end
+      }
+
+    it 'request will be 200 OK' do
+      expect(response.status).to eq 200 
+    end
+
+    it 'display the template' do
+      expect(response).to render_template :show
+    end
   end
 end
